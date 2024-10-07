dd7d647fdace132ed071d5a37c7055a5a9149f71
Simransingh726188 <simrankum6789@gmail.com>
2024-10-07 14:29:27 +0530

    f

---
+++ src/Component/Addiction/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+function Addiction(){
+    return(
+        <>
+            <Navbar></Navbar>
+            <main>
+                <div class="addiction-container">
+                    <h2>
+                        Addiction -
+                    </h2>
+                    <p>
+                    Addiction affects mental health as increased anxiety and depression substance use can exacerbate feelings of anxiety and depression, creating a cycle that makes recovery challenging. Addiction can impair judgement, memory, and decision-making, affecting daily functioning and relationaships.
+                    </p>
+                </div>
+            </main>
+            <section>
+            <button>Submit</button>
+            </section>
+        </>
+
+    )
+        
+}
+
+export default Addiction
