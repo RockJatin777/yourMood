58580a3703b2d94f442a5f4a80032130c4f1fe3c
RockJatin777 <150275513+RockJatin777@users.noreply.github.com>
2024-10-07 14:32:20 +0530

    f

--- src/Component/Home/index.js
+++ src/Component/Home/index.js
@@ -1,23 +1,26 @@
 import Navbar from "../Navbar";
+import './index.css'
 
 function Home(){
     return(
         <>
             <Navbar></Navbar>
-            <main>
+            <main className="home-container">
                 <h1>Welcome to the Your Mood App</h1>
-                <h1>Just Breathe <br/> And Relax</h1>
                 <p>A healthy mind has wealthy soul</p>
+                <h1>Just Breathe <br/> And Relax</h1>
+                
+                <section className="consent-container">
+                    <h1>How Are You Feeling Today</h1>
+                    <div className="btn-container">
+                        <button className="btn">Anxiety</button>
+                        <button className="btn">Stress</button>
+                        <button className="btn">Depression</button>
+                        <button className="btn">Mood Swing</button>
+                    </div>
+                </section>
             </main>
-            <section>
-                <h1>How Are You Feeling Today</h1>
-                <div>
-                    <button>Anxiety</button>
-                    <button>Stress</button>
-                    <button>Depression</button>
-                    <button>Mood Swing</button>
-                </div>
-            </section>
+            
         </>
     )
         
