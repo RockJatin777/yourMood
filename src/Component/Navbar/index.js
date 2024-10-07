f70280b213770f9db18b0cc1804293b8f7950563
RockJatin777 <150275513+RockJatin777@users.noreply.github.com>
2024-10-07 13:03:48 +0530

    Update index.js

---
+++ src/Component/Navbar/index.js
@@ -0,0 +1,18 @@
+function Navbar(){
+    return(
+        <header>
+            <img src="" />
+            <nav>
+                <select>
+                    <option>Home</option>
+                    <option>Conditions A-Z</option>
+                    <option>Living Well</option>
+                    <option>Psychology</option>
+                    <option>Therepy</option>
+                </select>
+            </nav>
+         </header>
+    )
+}
+
+export default Navbar
