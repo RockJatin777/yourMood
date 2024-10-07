29a239b6e05eacb6a49e2bf4e485401d14309903
Simransingh726188 <simrankum6789@gmail.com>
2024-10-07 16:27:39 +0530

    f

---
+++ src/Component/Footer/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+
+function Footer(){
+    return(
+        <footer>
+        <div>
+            <div class="footer">
+                <h3>Usefull Links</h3>
+                <ul class="links">
+                    <li>Home</li>
+                    <li>Addiction</li>
+                    <li>Anxiety</li>
+                    <li>Depression</li>
+                    <li>Get Help</li>
+                </ul>
+            </div>
+            <div class="footer-contact">
+                <h3>Contact Details</h3>
+                <ul class="links">
+                    <li>E-Mail : </li>
+                    <li>Phone No. : +91-81xxxxxx47</li>
+                    <li>Address : 121 East Main Street Riverhead, NY 11901, New York</li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <p>Copyrights &#169; All Rights Reserved - Your Mood || Designed With &#10084; By : Your-Mood Team</p>
+        </div>
+        </footer>
+    )
+}
+
+export default Footer
