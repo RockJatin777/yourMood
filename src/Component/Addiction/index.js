156e0f2fbd309efd63584cdef443b8160217c81b
Simransingh726188 <simrankum6789@gmail.com>
2024-10-07 15:32:34 +0530

    F

--- src/Component/Addiction/index.js
+++ src/Component/Addiction/index.js
@@ -16,46 +16,7 @@ function Addiction(){
                     <p>Addiction is a widely viewed as a brain disease due to how it impact the brain's reward, pleasure, and self-control circuits.</p>
                 </div>
             </main>
-            <section>
-                <h3>Understanding Addiction</h3>
-                <div>
-                    <div>
-                        <div>
-                            <img src={alcohol}></img>
-                            <h4>Addiction can disrupt personal relationships</h4>
-                        </div>
-
-                        <div>
-                            <img scr=""></img>
-                            <h4>Addiction can disrupt career opportunities</h4>
-                        </div>
-                        
-                        <div>
-                            <img scr=""></img>
-                            <h4>Addiction can often leads legal problem</h4>
-                        </div>
-                        
-                    </div>
-                        
-                    <div> 
-                        <div>
-                            <img scr=""></img>
-                            <h4>Addiction can often leads legal problem</h4>
-                        </div>
-                        
-                        <div>
-                            <img scr=""></img>
-                            <h4>Addiction can often leads legal problem</h4>
-                        </div>
-                        
-                        <div>
-                            <img scr=""></img>
-                            <h4>Addiction can often leads legal problem</h4>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
+    </>
 
     )
         
