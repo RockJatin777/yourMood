f838ffe0d3b5245c2d91d62894d3aa87a60e0381
Simransingh726188 <simrankum6789@gmail.com>
2024-10-07 14:51:39 +0530

    f

--- src/Component/Addiction/index.js
+++ src/Component/Addiction/index.js
@@ -11,10 +11,50 @@ function Addiction(){
                     <p>
                     Addiction affects mental health as increased anxiety and depression substance use can exacerbate feelings of anxiety and depression, creating a cycle that makes recovery challenging. Addiction can impair judgement, memory, and decision-making, affecting daily functioning and relationaships.
                     </p>
+                    <p>Addiction is a widely viewed as a brain disease due to how it impact the brain's reward, pleasure, and self-control circuits.</p>
                 </div>
             </main>
             <section>
-            <button>Submit</button>
+                <h3>Understanding Addiction</h3>
+                <div>
+                    <div>
+                        <div>
+                            <img src=""></img>
+                            <h4>Addiction can disrupt personal relationships</h4>
+                        </div>
+
+                        <div>
+                            <img scr=""></img>
+                            <h4>Addiction can disrupt career opportunities</h4>
+                        </div>
+                        
+                        <div>
+                            <img scr=""></img>
+                            <h4>Addiction can often leads legal problem</h4>
+                        </div>
+                        
+                    </div>
+                        
+                    <div> 
+                        <div>
+                            <img scr=""></img>
+                            <h4>Addiction can often leads legal problem</h4>
+                        </div>
+                        
+                        <div>
+                            <img scr=""></img>
+                            <h4>Addiction can often leads legal problem</h4>
+                        </div>
+                        
+                        <div>
+                            <img scr=""></img>
+                            <h4>Addiction can often leads legal problem</h4>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    
+                </div>
             </section>
         </>
 
