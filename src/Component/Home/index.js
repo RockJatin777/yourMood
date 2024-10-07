167308a677b0a089d225ae0c99274111a411ef63
RockJatin777 <150275513+RockJatin777@users.noreply.github.com>
2024-10-07 13:58:32 +0530

    Update index.js

--- src/Component/Home/index.js
+++ src/Component/Home/index.js
@@ -4,6 +4,20 @@ function Home(){
     return(
         <>
             <Navbar></Navbar>
+            <main>
+                <h1>Welcome to the Your Mood App</h1>
+                <h1>Just Breathe <br/> And Relax</h1>
+                <p>A healthy mind has wealthy soul</p>
+            </main>
+            <section>
+                <h1>How Are You Feeling Today</h1>
+                <div>
+                    <button>Anxiety</button>
+                    <button>Stress</button>
+                    <button>Depression</button>
+                    <button>Mood Swing</button>
+                </div>
+            </section>
         </>
     )
         
