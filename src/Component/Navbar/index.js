11fd266a3aeb70b2c0f86428ed4aac0bab0c34d6
RockJatin777 <rockjatin0748@gmail.com>
2024-10-08 03:41:40 +0530

    f

--- src/Component/Navbar/index.js
+++ src/Component/Navbar/index.js
@@ -1,47 +1,42 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation} from 'react-router-dom';
 
 import logo from '../Assets/logo.png';
 import './index.css';
 
 function Navbar() {
     const location = useLocation();
-    const navigate = useNavigate();
     const path = location.pathname;
-
-    const handleNavigate = (event) => {
-        const value = event.target.value;
-        if (value) {
-            navigate(value);
-        }
-    };
 
     return (
         <header>
             <img className='nav-logo' src={logo} alt="logo" />
             <nav>
                 <Link className='link' to={'/'}>
-                    <p className={path === '/' ? 'underline' : null}>Home</p>
+                    <p className={path === '/' ? 'underline' : 'hoverline'}>Home</p>
                 </Link>
-            
-                <select onChange={handleNavigate}>
-                    <option value='/'>Conditions</option>
-                    <option value="/addiction">Addiction</option>
-                    <option value="/depression">Depression</option>
-                    <option value="/anxiety">Anxiety</option>
-                </select>
+                <div className="menu-container">
+                    <p className={path === '/addiction' || path=== '/depression' || path === '/anxiety' ? 'drop-down underline' : 'drop-down hoverline'}>Conditions</p>
+                    <div className="condition-card">
+                        <Link className='link' to="/addiction"><p>Addiction</p></Link>
+                        <Link className='link' to="/depression"><p>Depression</p></Link>
+                        <Link className='link' to="/anxiety"><p>Anxiety</p></Link>
+                    </div>
+                </div>
 
                 <Link to={'/therepy'} className='link'>
-                    <p className={path === '/therepy' ? 'underline' : null}>Therepy</p>
+                    <p className={path === '/therepy' ? 'underline' : 'hoverline'}>Therepy</p>
                 </Link>
-                <select onChange={handleNavigate}>
-                    <option value='/'>Living Well</option>
-                    <option value="/stress">Manage Stress</option>
-                    <option value="/meditation">Meditation</option>
-                    <option value="/emotion">Emotion</option>
-                </select>
+                <div className="menu-container">
+                    <p className={path === '/emotion' || path=== '/stress' || path === '/meditation' ? 'drop-down underline' : 'drop-down hoverline'}>Living Well</p>
+                    <div className='condition-card'>
+                        <Link className='link' to="/stress"><p>Manage Stress</p></Link>
+                        <Link className='link' to="/meditation"><p>Medication</p></Link>
+                        <Link className='link' to="/emotion"><p>Emotion</p></Link>
+                    </div>
+                </div>
 
                 <Link to="/help" className='link'>
-                    <p className={path === '/help' ? 'underline' : null}>Get Help</p>
+                    <p className={path === '/help' ? 'underline' : 'hoverline'}>Get Help</p>
                 </Link>
             </nav>
         </header>
