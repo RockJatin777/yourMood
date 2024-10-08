1852605fe0ea1cea11f76f952dad4a582150cdef
Simransingh726188 <simrankum6789@gmail.com>
2024-10-08 11:35:47 +0530

    f

--- src/Component/Addiction/index.js
+++ src/Component/Addiction/index.js
@@ -14,11 +14,9 @@ function Addiction(){
                 <div className="addiction-content-container">
                     <h1 className="font-lg">Addiction </h1>
                     <p>Addiction affects mental health as increased anxiety and depression substance use can exacerbate feelings of anxiety and depression, creating a cycle that makes recovery challenging. Addiction can impair judgement, memory, and decision-making, affecting daily functioning and relationaships.</p>
-                    <h2>How can you break an addiction?</h2>
+
+                    <h2 className="font-lg">How can you break an addiction?</h2>
                     <p>The first step in overcoming addiction is to desire change and avoid the people, places and things which promote the addiction. Support groups, mentors, and 12 step programs provide help, while counselors, recovery centers, and physicians provide expertise and medication to treat cravings and the effects of withdrawal. Eating healthy and getting exercise also play an important role in recovery.</p>
-                </div>
-                <div>
-                    
                 </div>
             </main>
             <h1 className='exp-heading'>Explore Addiction</h1>
